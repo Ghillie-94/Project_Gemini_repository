8e734872755e2f8f7167076b5a5d2fe43a71b699
cameron boyle <cameronboyle7894@gmail.com>
2021-04-17 13:33:53 +0100

    Unity Visual asset import & Gemini movement script

---
+++ Project Gemini Project folder/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public variables
+    public float movementForceTurn = 7f;
+    public float movementForce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+        //condition: when the player presses D key...
+        if (Input.GetKey(KeyCode.D))
+        {
+            //Action: apply a force (move the player)    
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+            //Add a force to the rigidbody to move our player
+            ourRigidbody.AddForce(Vector2.right * movementForce);
+
+        }
+
+        //condition: when the player presses A key...
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Action: apply a force (move the player)    
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+            //Add a force to the rigidbody to move our player
+            ourRigidbody.AddForce(Vector2.left * movementForceTurn);
+
+        }
+        //condition: when the player presses W key...
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Action: apply a force (move the player)    
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+            //Add a force to the rigidbody to move our player
+            ourRigidbody.AddForce(Vector2.up * movementForceTurn);
+
+        }
+        //condition: when the player presses S key...
+        if (Input.GetKey(KeyCode.S))
+        {
+            //Action: apply a force (move the player)    
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+            //Add a force to the rigidbody to move our player
+            ourRigidbody.AddForce(Vector2.down * movementForceTurn);
+
+        }
+
+    }
+}
