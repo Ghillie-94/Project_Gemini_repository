567c60b49544cba780d17734f16ca2ff2f27ee25
cameron boyle <cameronboyle7894@gmail.com>
2021-05-04 17:09:05 +0100

    player firing script functional still to implement health system for enemies

---
+++ Project Gemini Project folder/Assets/scripts/PlayerDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+
+    //CONDITION: when the projectile hits a certain object type... (enemy)
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        //Check if the object we collided with has the tag we are looking for (Enemy)
+        if (otherCollider.CompareTag("Enemy"))
+        {
+            //perform our action
+            KillEnemy(otherCollider.gameObject);
+
+            Destroy(gameObject);
+        }
+    }
+
+    //ACTION: Destroy an object (enemy)
+    public void KillEnemy(GameObject enemy)
+    {
+        Destroy(enemy);
+          
+    }
+}
