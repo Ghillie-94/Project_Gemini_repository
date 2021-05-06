3d20fdbede55b286e87d7dac6440f754b066f370
cameron boyle <cameronboyle7894@gmail.com>
2021-05-06 12:26:20 +0100

    player health system part 1

---
+++ Project Gemini Project folder/Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // This function is NOT built into unity!
+    // It will only be called manually by our own code
+    // It must be marked public so other scripts can access it.
+    public void Kill()
+    {
+        //This will destroy the game object that this script is attatched to.
+        Destroy(gameObject);
+    }
+}
