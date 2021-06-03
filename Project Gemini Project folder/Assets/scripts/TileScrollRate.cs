265b0bae1afb034f8ccaf2e246f638ba1ac99d59
cameron boyle <cameronboyle7894@gmail.com>
2021-06-03 17:20:56 +0100

    scroll rate script wrote and applied, tiles rescaled

---
+++ Project Gemini Project folder/Assets/scripts/TileScrollRate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScrollRate : MonoBehaviour
+{
+    
+   public float scrollRate;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-scrollRate*Time.deltaTime,0,0);
+    }
+}
