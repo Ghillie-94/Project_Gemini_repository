880c1d8f51924740622b37d9adf5d600c9e9a779
cameron boyle <cameronboyle7894@gmail.com>
2021-06-07 19:40:39 +0100

    explosion rescaled and made into a prefab

---
+++ Project Gemini Project folder/Assets/scripts/Explosion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+   public GameObject projectilePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoExplosion();
+    }
+
+    // ACTION: fire a projectile
+    public void DoExplosion()
+    {
+        
+        //clone the projectile
+        //declare a variable to hold the cloned object
+        GameObject clonedProjectile;
+        // Use Instantiate to clone the projectile and keep the result in our variable
+        clonedProjectile = Instantiate(projectilePrefab);
+
+        //position the projectile on the player... tranform is the location of the script (the player object)
+        clonedProjectile.transform.position = transform.position; //optional: add an offset (use a public variable)
+
+        
+
+
+    }
+}
