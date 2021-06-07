320ad623dd3fcd9186a18af40184c38ef6997b4e
cameron boyle <cameronboyle7894@gmail.com>
2021-06-07 17:08:32 +0100

    Rocket spawner implemented

---
+++ Project Gemini Project folder/Assets/scripts/RocketSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketSpawner : MonoBehaviour
+{
+	public GameObject projectilePrefab;
+
+    private float timer = 4f;
+    
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            FireRocket();
+            timer = 4f;
+        }
+
+    }
+
+    // ACTION: fire a projectile
+    public void FireRocket()
+    {
+        
+        //clone the projectile
+        //declare a variable to hold the cloned object
+        GameObject clonedProjectile;
+        // Use Instantiate to clone the projectile and keep the result in our variable
+        clonedProjectile = Instantiate(projectilePrefab);
+
+        //position the projectile on the player... tranform is the location of the script (the player object)
+        clonedProjectile.transform.position = transform.position; //optional: add an offset (use a public variable)
+
+        
+
+
+    }
+  
+}
