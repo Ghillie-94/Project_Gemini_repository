18a28f4a0538e6707331d0dfef6532e8f4735570
cameron boyle <cameronboyle7894@gmail.com>
2021-06-15 13:59:51 +0100

    added camera scroll rate script

---
+++ Project Gemini Project folder/Assets/scripts/CameraScrollRate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScrollRate : MonoBehaviour
+{
+    public float scrollRate;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(scrollRate*Time.deltaTime,0,0);
+    }
+}
